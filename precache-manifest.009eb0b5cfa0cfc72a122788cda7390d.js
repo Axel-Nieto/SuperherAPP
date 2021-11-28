611e6f8a0a613f3388993fbfef9772f24c948c04
Axel Nieto <axelnieto616@gmail.com>
2021-11-28 07:28:26 -0300

    Updates

---
+++ precache-manifest.009eb0b5cfa0cfc72a122788cda7390d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b2d07b912ed50965d3329a04276ea00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1280c8f034b71e8ef190",
+    "url": "/static/css/2.fa43bc9e.chunk.css"
+  },
+  {
+    "revision": "189d331b54737ad46dac",
+    "url": "/static/css/main.d683c546.chunk.css"
+  },
+  {
+    "revision": "1280c8f034b71e8ef190",
+    "url": "/static/js/2.0686bd2c.chunk.js"
+  },
+  {
+    "revision": "119ff7b0e6dbb852f63360f69938dd73",
+    "url": "/static/js/2.0686bd2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a482139ab321aa0ef41",
+    "url": "/static/js/3.5491c425.chunk.js"
+  },
+  {
+    "revision": "189d331b54737ad46dac",
+    "url": "/static/js/main.6d4dbf98.chunk.js"
+  },
+  {
+    "revision": "d056a0d53ade11c714c3",
+    "url": "/static/js/runtime-main.ae6ec4a5.js"
+  },
+  {
+    "revision": "0678886c00cef749666b3de16859f317",
+    "url": "/static/media/bg-superhero2.0678886c.jpeg"
+  },
+  {
+    "revision": "fd7cf158f5cfe1ee7cfebc5586a79d6c",
+    "url": "/static/media/not-found.fd7cf158.png"
+  },
+  {
+    "revision": "849aaca6ceb1229d2eb449f1a2675232",
+    "url": "/static/media/super-logo.849aaca6.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/superherAPP/precache-manifest.3c7d13b5341423142b81115505008097.js"
+  "/precache-manifest.009eb0b5cfa0cfc72a122788cda7390d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/superherAPP/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
