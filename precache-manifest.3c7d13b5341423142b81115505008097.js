a7f69d4710d2ef64cfbcfb6869ae96adf9104b79
Axel Nieto <axelnieto616@gmail.com>
2021-11-28 07:18:29 -0300

    Updates

---
+++ precache-manifest.3c7d13b5341423142b81115505008097.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4053c18dd5d33fdef5ed5885ece46518",
+    "url": "/superherAPP/index.html"
+  },
+  {
+    "revision": "1280c8f034b71e8ef190",
+    "url": "/superherAPP/static/css/2.fa43bc9e.chunk.css"
+  },
+  {
+    "revision": "030f84113a304bf36983",
+    "url": "/superherAPP/static/css/main.2ccb35f2.chunk.css"
+  },
+  {
+    "revision": "1280c8f034b71e8ef190",
+    "url": "/superherAPP/static/js/2.0686bd2c.chunk.js"
+  },
+  {
+    "revision": "119ff7b0e6dbb852f63360f69938dd73",
+    "url": "/superherAPP/static/js/2.0686bd2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a482139ab321aa0ef41",
+    "url": "/superherAPP/static/js/3.5491c425.chunk.js"
+  },
+  {
+    "revision": "030f84113a304bf36983",
+    "url": "/superherAPP/static/js/main.6d4dbf98.chunk.js"
+  },
+  {
+    "revision": "1c96e95cf235c82583db",
+    "url": "/superherAPP/static/js/runtime-main.aaa61d0f.js"
+  },
+  {
+    "revision": "0678886c00cef749666b3de16859f317",
+    "url": "/superherAPP/static/media/bg-superhero2.0678886c.jpeg"
+  },
+  {
+    "revision": "fd7cf158f5cfe1ee7cfebc5586a79d6c",
+    "url": "/superherAPP/static/media/not-found.fd7cf158.png"
+  },
+  {
+    "revision": "849aaca6ceb1229d2eb449f1a2675232",
+    "url": "/superherAPP/static/media/super-logo.849aaca6.png"
+  }
+]);
